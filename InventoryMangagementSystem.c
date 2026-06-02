fc1f5e53b164aa4a434a577398416ef29019a0bb
areur <bmafeilx824@gmail.com>
2026-06-01 23:53:01 -0400

    -New feature: add items Lessons learned: C arguments arent objects it just copies the value, if you try to spam concatenate and don't keep a tight hold on the size of each string things go wrong

--- InventoryMangagementSystem.c
+++ InventoryMangagementSystem.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct { //typedef makes this struct a type for variables
     int id;
-    char * name;
+    char name[50];
 
     float price;
     int amount;
@@ -16,24 +18,54 @@ typedef struct {
 
 } inventory;
 
-inventory initInventory(int initialCapacity) {
-    inventory inv; 
-    inv.storageCapacity = initialCapacity;
+void initInventory(inventory *inv,int initialCapacity) {
+    inv->storageCapacity = initialCapacity;
+    inv->stockLevel = 0;
 
     //allocate memory for the item list
-    inv.items = (item *) malloc(sizeof(item) * initialCapacity);
+    inv->items = (item *)malloc(sizeof(item) * initialCapacity);
 
     //error handling for memory allocation
-    if (inv.items == NULL) {
+    if (inv->items == NULL) {
         printf("ERROR: Memory Allocation failed");
         exit(1);
     }
 
-    return inv;
 }
 
+void addItem(inventory *inv, int ID, char *name, float price, int amount) {
+    if (inv->stockLevel == inv->storageCapacity) { //no memory space, allocate more
+        inv->storageCapacity *= 3;
+        inv->items = realloc(inv->items, sizeof(item) * inv->storageCapacity);
+        printf("added more space guys");
+    }
+
+    item *currentItem = &(inv->items[inv->stockLevel]);
+
+    currentItem->id = ID;
+    strcpy(currentItem->name, name);
+    currentItem->price = price;
+    currentItem->amount = amount;
+    inv->stockLevel = (inv->stockLevel+amount);
+    
+    printf("Added Item: %s with id: %d\n",name ,ID);
+}
+
+void freeInventory(inventory *inv) {
+    free(inv->items);
+    inv->items = NULL;
+    inv->stockLevel = 0;
+    inv->storageCapacity = 0;
+}
 
 int main() {
+    inventory inv;
+    initInventory(&inv,50);
+
+    char name[50] = "son";
+    addItem(&inv ,1 ,name ,6.99 ,3);
+
+    freeInventory(&inv);
 
     return 0;
 }
